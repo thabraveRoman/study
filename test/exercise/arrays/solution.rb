40d3df2abb86502d135d92a8b6484a9290ddf779
Roman Zubanov <zubanovofficial@gmail.com>
2020-04-29 00:25:20 +0400

    my_solution_arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,12 +2,31 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.reduce { |acc, item| acc > item ? acc : item }
       end
 
-      def search(_array, _query)
-        0
-      end
+      
+      def binary_search(n, arr)
+        middle = arr[arr.length / 2]
+        i = 0
+        j = arr.length - 1
+
+        while i < j
+          if middle == n
+            return true
+          elsif middle < n
+            i = middle
+            middle = i + j / 2
+          else
+            p "Middle is greater than n"
+            j = middle
+            p "j: #{j}"
+            middle = i + j / 2
+            p "middle: #{middle}"
+          end
+        end
+        false
+      end 
     end
   end
 end
