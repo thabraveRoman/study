151e70018fa44cff4851a24e4c48975a45a4141f
Roman Zubanov <zubanovofficial@gmail.com>
2020-04-30 01:06:37 +0400

    array solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,9 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array = [3, 2, -8, 4, 100, -6, 7, 8, -99]
+        _max = array.reduce { |acc, item| acc > item ? acc : item }
+        array.map { |n| n > 0 ? _max : n }
       end
 
       def search(_array, _query)
