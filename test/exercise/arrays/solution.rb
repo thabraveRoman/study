37b7dd7f2ddc9da852b65c504383400863ca4ff1
Roman Zubanov <zubanovofficial@gmail.com>
2020-04-30 21:12:09 +0400

    remove array from func and rename _max to max

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,9 +2,9 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array = [3, 2, -8, 4, 100, -6, 7, 8, -99]
-        _max = array.reduce { |acc, item| acc > item ? acc : item }
-        array.map { |n| n > 0 ? _max : n }
+        
+        max = array.reduce { |acc, item| acc > item ? acc : item }
+        array.map { |n| n > 0 ? max : n }
       end
 
       def search(_array, _query)
