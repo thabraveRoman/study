1aba4fb4660155ebba1369f728a6313c8a574415
Roman Zubanov <zubanovofficial@gmail.com>
2020-05-11 23:17:04 +0400

    fix syntax

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,7 +3,7 @@ module Exercise
     class << self
       def replace(array)
         max = array.reduce { |acc, item| acc > item ? acc : item }
-        array.map { |n| n > 0 ? max : n }
+        array.map { |n| n.positive? ? max : n }
       end
 
       def search(array, query)
