45b85c00cf10edd5141ddff48416fe3d439bc8a1
Roman Zubanov <zubanovofficial@gmail.com>
2020-05-11 04:05:56 +0400

    arrays bin search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,14 +2,30 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        
         max = array.reduce { |acc, item| acc > item ? acc : item }
         array.map { |n| n > 0 ? max : n }
       end
 
-      def search(_array, _query)
-        0
-      end
+      def search(array, query)
+          left = 0
+          right = array.length - 1
+          if array.length == 0 || array[left] > query || array[right] < query
+            return -1
+          else
+            while left <= right
+              middle = (left + right) / 2
+              if array[middle] == query
+                return middle
+              elsif array[middle] > query
+                right = middle - 1
+              elsif array[middle] < query
+                left = middle + 1
+              else
+              end
+            end
+            return -1
+          end
+        end
     end
   end
 end
