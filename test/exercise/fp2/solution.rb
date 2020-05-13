13ed4f14c079dca91937922c6fd3e9c7f1b16b25
Roman Zubanov <zubanovofficial@gmail.com>
2020-05-13 05:04:48 +0400

    fp2-solution

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,19 +5,30 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each
+      def my_each(&func)
+        for element in self
+          func.call(element)
+        end
       end
 
       # Написать свою функцию my_map
-      def my_map
+      def my_map(&func)
+        result = MyArray.new
+        self.my_each { |element| result << func.call(element) }
+        return result 
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        result = MyArray.new
+        self.my_each { |element| element.nil? ? result : result << element }
+        return  result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil, &func)
+        acc = acc.nil ? element : func.call(acc, element)
+        return acc 
       end
     end
   end
