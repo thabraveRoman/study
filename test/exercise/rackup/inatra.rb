52dadf0dba406b71a701479067c166824145267c
Roman Zubanov <zubanovofficial@gmail.com>
2020-05-29 01:39:31 +0400

    inatra draft

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,7 +1,18 @@
 module Inatra
+  @@handlers {}
   class << self
-    def routes; end
+    def routes(&block)
+      instance_eval(&block)
+    end
 
-    def call(env); end
+    def get(&block)
+      @@store[path] = block
+    end
+
+    def call(env)
+    end
+
+    def method_missing(method_missing_name, path, &block)
+    end
   end
 end
