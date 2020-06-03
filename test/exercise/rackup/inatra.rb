db97275207fbda50df691876a4d39499600781f9
Roman Zubanov <zubanovofficial@gmail.com>
2020-06-03 20:07:05 +0400

    update solution

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,15 +1,18 @@
 module Inatra
-  @@handlers {}
+  @@handlers = {}
   class << self
     def routes(&block)
       instance_eval(&block)
     end
 
-    def get(&block)
-      @@store[path] = block
+    def get(path, &block)
+      @@handlers[path] = block
     end
 
     def call(env)
+      p env
+      hello = env['PATH_INFO']
+      get = env['REQUEST_METHOD']
     end
 
     def method_missing(missing_method_name, *args, &block)
